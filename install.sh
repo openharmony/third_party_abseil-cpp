64d624b6efc20177401a9b5a283bf812c6a0cf82
ganchuantao1 <ganchuantao1@huawei.com>
2025-02-13 09:30:29 +0800

    abseil 升级到20230802 版本     Signed-off-by:ganchuantao1<ganchuantao1@huawei.com>
    Signed-off-by: ganchuantao1 <ganchuantao1@huawei.com>

--- install.sh
+++ install.sh
@@ -8,10 +8,18 @@
 
 set -e
 cd $1
+{
+flock -x 100
 if [ -d "abseil-cpp" ];then
     rm -rf abseil-cpp
 fi
-tar zxvf abseil-cpp-20220623.1.tar.gz
-mv abseil-cpp-20220623.1 abseil-cpp
+tar zxvf abseil-cpp-20230802.1.tar.gz
+mv abseil-cpp-20230802.1 abseil-cpp
 cd $1/abseil-cpp
+patch -p1 < $1/abseil-cpp-20210324.2-sw.patch
+patch -p1 < $1/0001-add-loongarch-suopport-for-abseil-cpp.patch
+patch -p1 < $1/0002-PR-1644-unscaledcycleclock-remove-RISC-V-support.patch
+patch -p1 < $1/fix-mingw-complier-error.patch
+flock -u 100
+} 100<>$1/lock_file.lock
 exit 0
